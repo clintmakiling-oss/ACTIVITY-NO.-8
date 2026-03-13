4a0f0cf825296671dec862504d89ec69f81ac365
clintmakiling-oss <clintmakiling@gmail.com>
2026-03-14 00:41:23 +0800

    Add files via upload

---
+++ HANDS-ON EXERCISE NO. 8 - PHP/NO. 1/index2.php
@@ -0,0 +1,15 @@
+<?php include './layout/head.php'; ?>
+
+<div class="container">
+    <h1>Welcome to Our Website</h1>
+    <p>This is the home page of our PHP web application. Here you can find information about our services and navigate to different sections.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    
+    <div class="nav">
+        <a href="./register.php">Register</a> |
+        <a href="./login.php">Login</a> |
+        <a href="./forgot_password.php">Forgot Password</a>
+    </div>
+</div>
+
+<?php include './layout/foot.php'; ?>
